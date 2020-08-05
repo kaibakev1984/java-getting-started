f6afc45a9edfc39f90e2ee92e1083bc7198d9d79
Kevin Vasquez <kvasquez@fi.uba.ar>
2020-08-05 00:51:24 -0300

    refactorNumerosRomanos): se cambio por sumarUnificado

--- src/main/java/poo/coders/NumerosRomanos.java
+++ src/main/java/poo/coders/NumerosRomanos.java
@@ -2,19 +2,22 @@ package poo.coders;
 
 public class NumerosRomanos {
     public String convertirARomanos(int numeroNatural) {
-        if(numeroNatural <= 3) {
-            return sumarI(numeroNatural);
-        }
         switch (numeroNatural) {
             case 4: return "IV";
-            case 5: return "V";
+            case 9: return "IX";
         }
-        return null;
+        return sumarUnificado(numeroNatural);
     }
 
-    private String sumarI(int numeroNatural) {
+    private String sumarUnificado(int numeroNatural ) {
         String resultado = "";
-        for (int i = 1; i <= numeroNatural; i++) {
+        int inicio = 0;
+        int fin = numeroNatural;
+        if (numeroNatural > 4) {
+            inicio = 5;
+            resultado += "V";
+        }
+        for (int i = inicio; i < numeroNatural; i++) {
             resultado += "I";
         }
         return resultado;
--- src/test/java/poo/coders/NumerosRomanosTest.java
+++ src/test/java/poo/coders/NumerosRomanosTest.java
@@ -64,5 +64,4 @@ public class NumerosRomanosTest {
         assertEquals(romano, "IX");
     }
 
-
 }
